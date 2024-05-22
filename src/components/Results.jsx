cad75ae69035230fb0bfd58dfce304ad7d35283b
MrKaishKhan <muhmmadkaishkhan@gmail.com>
2024-05-22 22:51:06 +0530

    style add

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import { calculateInvestmentResults } from '../util/investment'
 
 const Results = ({input}) => {
-  console.log(input);
+ 
+    const resultData = calculateInvestmentResults(input) ;
+    const initialInvestment = resultData[0].valueEndOfYear - resultData[0].interest - resultData[0].annualInvestment;
+    
     return (
-    <div>
-      <p>Results....</p>
-    </div>
+        <table id='result'>
+          <thead>
+            <tr>
+                <th>Year</th>
+                <th>Investment Value</th>
+                <th>Interest (Year) </th>
+                <th>Total Interest  </th>
+                <th>Investment Capital  </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {resultData.map(yearData => {
+
+                const totalInterest = yearData.valueEndOfYear - yearData.annualInvestment * yearData.year;
+                return <tr key={yearData.year}>
+                    <td>{yearData.year}</td>
+                    <td>{formatter.format(yearData.valueEndOfYear)}</td>
+                    <td>{formatter.format(yearData.interest)}</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+
+            })}
+          </tbody>
+        </table>
+    
   )
 }
 
